52b9c35088822dbd1d20bc009ae8853d24426a17
junior cueva <junior.cueva@tecsup.edu.pe>
2026-08-19 09:32:04 -0500

    Initial Commit

---
+++ Laboratorio00/Laboratorio00App.swift
@@ -0,0 +1,17 @@
+//
+//  Laboratorio00App.swift
+//  Laboratorio00
+//
+//  Created by Tecsup on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Laboratorio00App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
